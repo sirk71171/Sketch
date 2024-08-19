613c60f79cce757f034442889d2ed941d06a6f97
TrapaZoid <git@github.com:sirk71171>
2024-08-18 23:44:50 -0600

    created while loop to prompt user for number between 1 and 100 until user meets condition. Added code that creates random colors on mouse over repeatedly.

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ var container = document.querySelector("#container");
 const iput = document.querySelector("#iput");
 const select = document.querySelector("#select");
 let STAT = "not-clicked";
-const input = window.prompt("Enter a number between 1 and 100: ");
+let input = 0; 
+while (input <= 0 || input > 100) {
+    input = window.prompt("Enter a number between 1 and 100: ");
+    }
 
 document.addEventListener("DOMContentLoaded", () => {
     STAT = "clicked";
@@ -11,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     for (i = 1; i <= input; i++) {
         const rows = document.createElement("div");
         rows.setAttribute("style", "display:flex;flex:1;height:100%;width:100%;");
-        rows.setAttribute("class", `boxrow${i}`);
+        rows.setAttribute("id", `boxrow${i}`);
         container.appendChild(rows);
         for (j = 1; j <= input; j++) {
             const boxes = document.createElement("div");
@@ -24,21 +27,23 @@ document.addEventListener("DOMContentLoaded", () => {
 function assignToElement () {
     document.onmouseover = function(e) {
         val = e.target.id;
-        if (val !== "select" && val !== "iput") {
-            document.getElementById(val).setAttribute("style", "display:flex;flex-basis:auto;flex-grow:1;flex-shrink:1;align-items:stretch;background-color:black;");
+        if (val !== "select" && val !== "iput" && val !== "container") {
+            document.getElementById(val).setAttribute("style", `display:flex;flex-basis:auto;flex-grow:1;flex-shrink:1;align-items:stretch;background-color:rgb(${colorVal()},${colorVal()},${colorVal()});`);
         }
-        
     }
 }
-container.addEventListener("DOMContentLoaded", assignToElement());
+function colorVal () {
+    return (Math.round(Math.random()*256));
+}
+container.addEventListener("mouseover", assignToElement());
 select.addEventListener("click", () => {
     for (i = 1; i <= input; i++) {
-        boxElement = document.getElementById(`boxrow${i}`);
-        boxElement.remove;
-        for (j = 1; j <= input; j++) {
-            element = document.getElementById(`box${i}*${j}`);
-            element.remove;
-
+        const boxElement = document.getElementById(`boxrow${i}`);
+        console.log(boxElement);
+        if (boxElement) {
+            boxElement.remove();
         }
+
     }
+    window.location.reload();
 })
